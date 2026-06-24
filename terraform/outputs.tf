2116594fac6d4b8d7f68910504ff0733807a8ecd
Timon Weides <timonweides@Mac.fritz.box>
2026-06-24 20:42:49 +0200

    Terraform Output erweitert

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,19 @@
 output "bucket_name" {
-  value = aws_s3_bucket.website.bucket
+  description = "S3 Bucket Name"
+  value       = aws_s3_bucket.website.bucket
+}
+
+output "cloudfront_id" {
+  description = "CloudFront Distribution ID — für Cache Invalidierung"
+  value       = aws_cloudfront_distribution.website.id
+}
+
+output "cloudfront_domain" {
+  description = "CloudFront Domain"
+  value       = "https://${aws_cloudfront_distribution.website.domain_name}"
+}
+
+output "website_url" {
+  description = "Öffentliche URL"
+  value       = "https://timonweides.de"
 }
